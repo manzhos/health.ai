142d83990a66cace43cfede6585f684745733024
Vladimir Manzhos <manzhos@gmail.com>
2022-10-10 19:48:14 +0200

    deleted:    client/README.md 	modified:   client/package-lock.json 	modified:   client/package.json 	modified:   client/public/index.html 	deleted:    client/src/App.css 	modified:   client/src/App.js 	new file:   client/src/component/Login.js 	new file:   client/src/css/style.css 	deleted:    client/src/logo.svg

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,14 @@
-import React, {useState} from 'react'
-import axios from 'axios'
-import './App.css'
+import React, {useState} from 'react';
+import axios from 'axios';
+import './css/style.css'
+import './component/Login'
+import Login from './component/Login';
 
 function App() {
-  const [message, setMessage] = useState('hello')
-
-  const buttonHandler = () => {
-    console.log('hi');
-    const src = 'http://localhost:3300/hi'
-    axios.get(src,{
-      headers: {"Content-Type": "application/json"},
-    }).then((res) => {
-      console.log(res)
-      setMessage(res.data)
-    })
-  }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          {message}
-        </p>
-        <button onClick={buttonHandler}>say 'hi'</button>
-      </header>
+      <Login />
     </div>
   );
 }
