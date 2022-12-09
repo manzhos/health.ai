e99e262a3f88c23fa5c4e14fcc0fa67f1df9e53b
Vladimir Manzhos <manzhos@gmail.com>
2022-12-09 17:40:15 +0100

    consult form

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,20 @@ import React, { useEffect } from 'react';
 // routes
 import Router from './routes';
 // theme
+// import { createTheme, ThemeProvider } from '@mui/material';
 import ThemeProvider from './theme';
 import './css/style.css';
 // components
 import ScrollToTop from './components/ScrollToTop';
 import { BaseOptionChartStyle } from './components/chart/BaseOptionChart';
+
+// const theme = createMuiTheme({
+//   typography: {
+//     fontFamily: [
+//       'Poppins',
+//       'regular',
+//     ].join(','),
+//   },});
 
 // ----------------------------------------------------------------------
 
